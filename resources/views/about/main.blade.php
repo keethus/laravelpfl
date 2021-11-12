b1463db68b7dbfcb3497c708a647fd0a721b3ada
keethus <kabarbars@gmail.com>
2021-11-12 18:10:32 +0200

    upgrade about page

--- resources/views/about/main.blade.php
+++ resources/views/about/main.blade.php
@@ -12,16 +12,13 @@
                 <div class="col">
                     <i class="fas fa-globe-europe about-icons"></i><span class="about-location">Latvia, Cēsis</span>
                 </div>
-                <div class="col">
-
-                </div>
             </div>
             <div class="row about-info-section">
                 <div class="col">
-                    <span class="about-info-text">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Atque dicta incidunt vitae? Blanditiis deserunt, quia. At autem deserunt doloribus illum iusto provident rem sed? Aut commodi cum cumque ipsum obcaecati odio officiis possimus quas sed tempora tempore vitae, voluptas voluptatibus. Et, eum impedit in magnam nam necessitatibus odio quo rerum?</span>
+                    <lottie-player class="about-animation" src="https://assets5.lottiefiles.com/packages/lf20_bqyrn8up.json" background="transparent" speed="1" style="width: 500px; height: 500px;" loop autoplay></lottie-player>
                 </div>
                 <div class="col">
-                    <lottie-player class="about-animation" src="https://assets10.lottiefiles.com/packages/lf20_xchcab8w.json" background="transparent" speed="1" style="width: 400px; height: 400px;" loop autoplay></lottie-player>
+                    <span class="about-info-text">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Atque dicta incidunt vitae? Blanditiis deserunt, quia. At autem deserunt doloribus illum iusto provident rem sed? Aut commodi cum cumque ipsum obcaecati odio officiis possimus quas sed tempora tempore vitae, voluptas voluptatibus. Et, eum impedit in magnam nam necessitatibus odio quo rerum?</span>
                 </div>
             </div>
         </div>
