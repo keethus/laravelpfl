fc858714d5ee4bfa3b661fb9bfede6a2f0120752
keethus <kabarbars@gmail.com>
2021-11-12 13:14:14 +0200

    secure asset on images

--- resources/views/home/index.blade.php
+++ resources/views/home/index.blade.php
@@ -15,7 +15,7 @@
                         <div class="w-100"></div>
                         <div class="landing-links row" >
                             <div class="col-sm flex-fill " style="--animate-delay: 0.1s;">
-                                <a class="" href="https://www.instagram.com/kabarbars/" target="_blank"><i class="fab fa-instagram animate__animated animate__bounceIn animate__delay-1s"></i></a>
+                                <a href="https://www.instagram.com/kabarbars/" target="_blank"><i class="fab fa-instagram animate__animated animate__bounceIn animate__delay-1s"></i></a>
                                 <a href="https://www.facebook.com/zeatzijs" target="_blank"><i class="fab fa-facebook-square animate__animated animate__bounceIn animate__delay-2s"></i></a>
                                 <a href="https://twitter.com/keeethus" target="_blank"><i class="fab fa-twitter animate__animated animate__bounceIn animate__delay-3s"></i></a>
                                 <a href="https://discord.com/channels/@me/keethus#6634" target="_blank"><i class="fab fa-discord animate__animated animate__bounceIn animate__delay-4s"></i></a>
